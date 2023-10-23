4441ba60fd7418546620f4c00948517a157cc60e
Alexander Du <du.alexander123@gmail.com>
2023-10-23 00:06:20 -0700

    clone + initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<nav>
+	<div>
+		<div class="links">
+			<a href="/">Latest Issue</a>
+			<a href="/about">About</a>
+			<a href="/issues">Past Issues</a>
+			<a href="/weekly-woof">The Weekly Woof</a>
+			<a href="/contact">Contact</a>
+		</div>
+		<hr />
+		<h1>THE INTERLAKE INQUIRER</h1>
+		<h2>The Official Student Newspaper of Interlake High School</h2>
+	</div>
+	<img class="logo" src="logo.png" alt="Logo of Interlake Inquierer" />
+</nav>
+
+<slot />
+
+<style>
+	.logo {
+		width: 150px;
+		height: 150px;
+		border-radius: 50%;
+	}
+	.links {
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+	}
+	.links a {
+		margin: 10px 1em;
+		color: black;
+		text-decoration: none;
+	}
+
+	hr {
+		max-width: 500px;
+	}
+
+	h1 {
+		text-align: center;
+		font-size: 2.5em;
+		margin-bottom: 10px;
+	}
+
+	h2 {
+		text-align: center;
+	}
+</style>
---
+++ src/routes/weekly-woof/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Weekly Woof</h1>
