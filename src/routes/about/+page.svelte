79b943a39c700f7752815646741500b452cfcc10
Alexander Du <du.alexander123@gmail.com>
2023-10-24 18:18:48 -0700

    about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,24 @@
+<script>
+	import Bio from '/src/bio/bio.json';
+
+	const editors = Bio.editors;
+	const contributors = Bio.contributors;
+
+	console.log(editors);
+</script>
+
 <h1>About</h1>
+
+<h1>Editors</h1>
+{#each editors as editor}
+	<h2>{editor.name}</h2>
+	<h3>{editor.position}</h3>
+	<p>{editor.bio}</p>
+{/each}
+
+<h1>Contributors</h1>
+{#each contributors as contributor}
+	<h2>{contributor.name}</h2>
+	<h3>{contributor.position}</h3>
+	<p>{contributor.bio}</p>
+{/each}
