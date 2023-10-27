55645cb7a509ce6d5826c7e89272e24467fb0b1a
Alexander Du <du.alexander123@gmail.com>
2023-10-27 00:42:27 -0700

    fix

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,6 +1,5 @@
-<script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
-
 <svelte:head>
 	<meta name="robots" content="noindex" />
 	<title>Admin Dashboard</title>
+	<script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
 </svelte:head>
