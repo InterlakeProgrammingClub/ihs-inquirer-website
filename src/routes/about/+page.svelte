d84fdb77e914bd7cc833c3464e7dec875c8bde04
refact0r <yifanw06@gmail.com>
2023-11-10 13:26:38 -0800

    staff bios

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,12 +2,15 @@
 	export let data;
 </script>
 
-<h1>Staff</h1>
-<div class="editors">
+<div class="content">
+	<h1>Staff</h1>
 	{#each data.bios as staff}
 		<div class="staff">
-			<img src={staff.photo} alt={staff.title} />
-
+			{#if !staff.photo}
+				<div class="photo placeholder" />
+			{:else}
+				<img class="photo" src={staff.photo} alt={staff.title} />
+			{/if}
 			<div class="bio">
 				<h2>{staff.title}</h2>
 				<h3>{staff.role}</h3>
@@ -18,4 +21,41 @@
 </div>
 
 <style lang="scss">
+	.content {
+		max-width: 50rem;
+		margin: 0 auto;
+	}
+
+	.staff {
+		display: flex;
+		gap: 3rem;
+		margin: 2.5rem 0;
+
+		h2 {
+			margin: 0;
+		}
+
+		p {
+			margin: 0;
+		}
+
+		h3 {
+			margin: 1rem 0;
+		}
+	}
+
+	.photo {
+		max-width: 10rem;
+		width: 100%;
+		height: fit-content;
+		border-radius: 0.6rem;
+		aspect-ratio: 4/5;
+		object-fit: cover;
+	}
+
+	.photo.placeholder {
+		background: var(--bg-1);
+		filter: brightness(0.9);
+		height: 100%;
+	}
 </style>
